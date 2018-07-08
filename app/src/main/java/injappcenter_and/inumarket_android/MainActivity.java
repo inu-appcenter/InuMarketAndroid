10cbf411d16f8d1124f3265f5f697d9c34348652
GowoonJ <jgw971229@naver.com>
2018-07-08 17:51:05 +0900

    프로젝트 재업로드

---
+++ app/src/main/java/injappcenter_and/inumarket_android/MainActivity.java
@@ -0,0 +1,13 @@
+package injappcenter_and.inumarket_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
